4e52dcb8078a00fe50339797b0a3cf6f435b8400
Oleg_Muzyria <olegmuzira@gmail.com>
2021-09-04 18:45:05 +0300

    - Updated test

--- src/main/java/com/bookdepository/pages/desktop/fragments/NavigationBar.java
+++ src/main/java/com/bookdepository/pages/desktop/fragments/NavigationBar.java
@@ -1,48 +1,45 @@
 package com.bookdepository.pages.desktop.fragments;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import com.bookdepository.driver.DriverManager;
 import com.bookdepository.pages.abstractclasses.fragment.AbstractFragment;
 import com.bookdepository.pages.desktop.pages.ContactUsPage;
 import com.bookdepository.pages.desktop.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 
 public class NavigationBar extends AbstractFragment {
 
-	@FindBy(css = ".user-nav-wrap")
-	WebElement navigationBarCoreElement;
+    @FindBy(css = ".user-nav-wrap")
+    WebElement navigationBarCoreElement;
 
-	@FindBy(css = ".user-nav-wrap  a[href='/contactus']")
-	WebElement contactUsButtonElement;
+    @FindBy(css = ".user-nav-wrap  a[href='/contactus']")
+    WebElement contactUsButtonElement;
 
-	@FindBy(css = ".user-nav-wrap  .home-icon-link")
-	WebElement homeButtonElement;
+    @FindBy(css = ".user-nav-wrap  .home-icon-link")
+    WebElement homeButtonElement;
 
-	@FindBy(css = "a[href=\"/account\"]")
-	WebElement myAccountButton;
+    @FindBy(css = "a[href=\"/account\"]")
+    WebElement myAccountButton;
 
-	public NavigationBar(WebDriver driver) {
-		super(driver);
-		setRootElement(navigationBarCoreElement);
-	}
+    public NavigationBar(WebDriver driver) {
+        super(driver);
+        setRootElement(navigationBarCoreElement);
+    }
 
-	public ContactUsPage pressContactUsButton(WebDriver driver){
-		contactUsButtonElement.click();
-		return new ContactUsPage(driver);
-	}
+    public ContactUsPage pressContactUsButton(WebDriver driver) {
+        contactUsButtonElement.click();
+        return new ContactUsPage(driver);
+    }
 
-	public HomePage pressHomeButton(WebDriver driver){
-		homeButtonElement.click();
-		return new HomePage(driver);
-	}
+    public HomePage pressHomeButton(WebDriver driver) {
+        homeButtonElement.click();
+        return new HomePage(driver);
+    }
 
-	public boolean isAccountButtonPresent() {
-		find(myAccountButton);
-		return true;
-	}
+    public boolean isAccountButtonPresent() {
+        find(myAccountButton);
+        return true;
+    }
 
 }
